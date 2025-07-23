276d4cd77a052aa7fc874e284d3822f6fdeb7212
Macintosh Chea <chea.freelance@gmail.com>
2025-07-24 03:22:09 +0400

    Update ProductAdapter.kt

--- ProductAdapter.kt
+++ ProductAdapter.kt
@@ -1,1 +1,7 @@
 app/src/main/java/com/chea/myshopapp/ProductAdapter.kt
+import android.widget.Button
+inner class ProductViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val productNameText: TextView = view.findViewById(R.id.productName)
+    val productPriceText: TextView = view.findViewById(R.id.productPrice)
+    val addToCartButton: Button = view.findViewById(R.id.addToCartButton)
+}
