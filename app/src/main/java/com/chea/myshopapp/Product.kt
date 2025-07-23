d548a6792c63e747c82ed529adb7ce50b27c5162
Macintosh Chea <chea.freelance@gmail.com>
2025-07-24 02:38:19 +0400

    Update Product.kt

--- app/src/main/java/com/chea/myshopapp/Product.kt
+++ app/src/main/java/com/chea/myshopapp/Product.kt
@@ -1,7 +1,34 @@
 package com.chea.myshopapp
 
-data class Product(
-    val name: String,
-    val price: Double,
-    val imageResId: Int
-)
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var productRecyclerView: RecyclerView
+    private lateinit var productAdapter: ProductAdapter
+    private lateinit var productList: List<Product>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        productRecyclerView = findViewById(R.id.productRecyclerView)
+        productRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        productList = getSampleProducts()
+        productAdapter = ProductAdapter(productList)
+        productRecyclerView.adapter = productAdapter
+    }
+
+    private fun getSampleProducts(): List<Product> {
+        return listOf(
+            Product("T-Shirt", 9.99, R.mipmap.ic_launcher),
+            Product("Jeans", 19.99, R.mipmap.ic_launcher),
+            Product("Sneakers", 29.99, R.mipmap.ic_launcher),
+            Product("Hat", 5.99, R.mipmap.ic_launcher)
+        )
+    }
+}
